0bf4ea54396a492179a04676f57d1997a4093847
Anna Oksanichenko <oksanichenkoann@gmail.com>
2021-06-22 11:07:36 +0300

    WIP

--- Chat/ChatTableViewCell.swift
+++ Chat/ChatTableViewCell.swift
@@ -2,30 +2,34 @@
 //  ChatTableViewCell.swift
 //  Chat
 //
-//  Created by Anna Oksanichenko on 10.06.2021.
+//  Created by Anna Oksanichenko on 18.06.2021.
 //
 
 import UIKit
 
 class ChatTableViewCell: UITableViewCell {
-
+    
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var accountImage: UIImageView!
     @IBOutlet weak var messageLabel: UILabel!
     
     static let reuseIdentifier = "ChatTableViewCell"
     static let nibName = reuseIdentifier
-    
+
     override func awakeFromNib() {
         super.awakeFromNib()
-
-        self.messageLabel.sizeToFit()
-        self.messageLabel.numberOfLines = 0
-
+        // Initialization code
     }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
     func fill(text: String, index: Int) {
-        profileImage.image = index % 2 == 0 ? UIImage(named: "cat") : UIImage(named: "dog")
-        nameLabel.text = index % 2 == 0 ? "Kate" : "Luke"
+        accountImage.image = index % 2 == 0 ? UIImage(named: "cat") : UIImage(named: "dog")
+        nameLabel.text = index % 2 == 0 ? "Kate" : "Tony"
         messageLabel.text = text
 
     }
--- Chat/ChatViewController.swift
+++ Chat/ChatViewController.swift
@@ -7,39 +7,52 @@
 
 import UIKit
 
-
 class ChatViewController: UIViewController {
     
-    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var tableView: UITableView!
-    @IBOutlet weak var msgTextview: UITextView!
-    
-    var test = ["one", "two", "three", "four"]
+    var test = ["one", "labeljernfwejrnrvkjern bjvkqberkjvbnefkjbn vkejrbvlkjeknjehb qefljhbvqelj rbvljherbnvjlheqbrvjbenr;clv efwbv rkbcv;kjwkdb nv;qbrwkfbq;wrkbfq;krw bv;kbf;kvcjbqrewkjqejbfngkwerh vbweiprbhvipfhbpivhgpiw iprtbgpweijrtnjvnwefinwpeir", "index % 2 == 0 ?", "four", "five", "siiiix"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         let nib = UINib(nibName: ChatTableViewCell.reuseIdentifier, bundle: nil)
         tableView.register(nib, forCellReuseIdentifier: ChatTableViewCell.reuseIdentifier)
+       
+        tableView.estimatedSectionFooterHeight = 70.0
+        tableView.sectionFooterHeight = UITableView.automaticDimension
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 84.0
         tableView.delegate = self
         tableView.dataSource = self
-        msgTextview.delegate = self
-        msgTextview.isScrollEnabled = false
-        
         self.title = "Chat"
-        //        tableView.estimatedRowHeight = UITableView.automaticDimension
-        //        tableView.rowHeight = UITableView.automaticDimension
         registerForKeyboardNotifications()
+        
+        //Option 1
+//        let customView = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 50))
+//        customView.backgroundColor = UIColor.red
+//        let msgTextview = UITextView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 25))
+//        msgTextview.backgroundColor = .green
+//        msgTextview.delegate = self
+//        msgTextview.font = UIFont.systemFont(ofSize: 19.0)
+//        msgTextview.isScrollEnabled = false
+//       // msgTextview.translatesAutoresizingMaskIntoConstraints = false
+//        customView.addSubview(msgTextview)
+//        tableView.tableFooterView = customView
+
     }
     deinit {
         deregisterFromKeyboardNotifications()
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+            tableView.reloadData()
+        }
 }
 
 extension ChatViewController: UITableViewDelegate, UITableViewDataSource {
-    
+   
     func adjustUITextViewHeight(arg : UITextView) {
-        arg.translatesAutoresizingMaskIntoConstraints = true
+        arg.translatesAutoresizingMaskIntoConstraints = false
         arg.sizeToFit()
         arg.isScrollEnabled = false
     }
@@ -50,36 +63,80 @@ extension ChatViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: ChatTableViewCell.reuseIdentifier, for: indexPath) as! ChatTableViewCell
-        cell.fill(text: test[indexPath.row], index: indexPath.row)
+        let message = test[indexPath.row]
+        cell.fill(text: message, index: indexPath.row)
         return cell
     }
     
-    //    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
-    //        return 84.0
-    //    }
-    //    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-    //        return UITableView.automaticDimension
-    //    }
+    private func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+        }
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
         return 84.0
     }
+
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+
+    func tableView(_ tableView: UITableView, estimatedHeightForFooterInSection section: Int) -> CGFloat {
+        print("est")
+        return 70.0
+    }
+
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let custom = UIView()
+        custom.backgroundColor = .brown
+        let msgTextview  = UITextView()
+        msgTextview.backgroundColor = .green
+        msgTextview.delegate = self
+        msgTextview.font = UIFont.systemFont(ofSize: 20.0)
+        msgTextview.translatesAutoresizingMaskIntoConstraints = false
+        msgTextview.isScrollEnabled = false
+        custom.addSubview(msgTextview)
+
+        msgTextview.leadingAnchor.constraint(equalTo: custom.leadingAnchor).isActive = true
+        msgTextview.trailingAnchor.constraint(equalTo: custom.trailingAnchor).isActive = true
+        msgTextview.topAnchor.constraint(equalTo: custom.topAnchor).isActive = true
+        msgTextview.bottomAnchor.constraint(equalTo: custom.bottomAnchor).isActive = true
+
+        return custom
+    }
+
 }
 
 extension ChatViewController: UITextFieldDelegate, UITextViewDelegate {
     
     func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
-        if text == "\n" {
+
+        if text == "\n" && text.contains("\n") {
             guard let newMsg = textView.text else {
+                print("cant identifite msg")
                 return false
             }
             test.append(newMsg)
+            print(newMsg)
             textView.text = ""
+
             tableView.reloadData()
             scrollToBottom()
             return false
         }
         return true
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        textView.isScrollEnabled = false
+        print(textView.bounds.height)
+        tableView.reloadData()
+    }
+    
+    func scrollToBottom(){
+        DispatchQueue.main.async {
+            let indexPath = IndexPath(row: self.test.count-1, section: 0)
+            self.tableView.scrollToRow(at: indexPath, at: .bottom, animated: true)
+        }
     }
     
     func registerForKeyboardNotifications() {
@@ -92,46 +149,28 @@ extension ChatViewController: UITextFieldDelegate, UITextViewDelegate {
         NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
-    @objc func keyboardWillShown(notification: NSNotification) {
-        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
-            if self.view.frame.origin.y == 0 {
-                self.view.frame.origin.y -= keyboardSize.height
+        @objc
+        func keyboardWillShown(notification: NSNotification) {
+            print("shown")
+            guard let info = notification.userInfo,
+                  let keyboardSize = (info[UIResponder.keyboardFrameEndUserInfoKey ] as? NSValue)?.cgRectValue.size else {
+                print("return")
+                return
             }
+            
+            let currentScrollHeight = keyboardSize.height
+            print(currentScrollHeight)
+            let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: currentScrollHeight, right: 0.0)
+            tableView.contentInset = contentInsets
+            tableView.scrollIndicatorInsets = contentInsets
+            print("content insets")
         }
-    }
     
-    @objc func keyboardWillBeHidden(notification: NSNotification) {
-        if self.view.frame.origin.y != 0 {
-            self.view.frame.origin.y = 0
+        @objc
+        func keyboardWillBeHidden(notification: NSNotification) {
+            let contentInsets: UIEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+            self.tableView.contentInset = contentInsets
+            self.tableView.scrollIndicatorInsets = contentInsets
         }
-    }
-    
-    //    @objc
-    //    func keyboardWillShown(notification: NSNotification) {
-    //        guard let info = notification.userInfo,
-    //              let keyboardSize = (info[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.size else {
-    //            return
-    //        }
-    //
-    //        let currentScrollHeight = keyboardSize.height
-    //        let contentInsets: UIEdgeInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: currentScrollHeight, right: 0.0)
-    //        self.scrollView.contentInset = contentInsets
-    //        self.scrollView.scrollIndicatorInsets = contentInsets
-    //
-    //    }
-    //
-    //    @objc
-    //    func keyboardWillBeHidden(notification: NSNotification) {
-    //        let contentInsets: UIEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
-    //        self.scrollView.contentInset = contentInsets
-    //        self.scrollView.scrollIndicatorInsets = contentInsets
-    //    }
-    
-    func scrollToBottom(){
-        DispatchQueue.main.async {
-            let indexPath = IndexPath(row: self.test.count-1, section: 0)
-            self.tableView.scrollToRow(at: indexPath, at: .bottom, animated: true)
-        }
-    }
 }
 
